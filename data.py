600e272c2533cc95c4b9dfc420d5e0695d17dac6
Alessandra A <alessandraalvarado.99@gmail.com>
2023-08-27 00:29:52 -0600

    first commit

---
+++ data.py
@@ -0,0 +1,31 @@
+question_data = [
+  {"category":"Science: Mathematics"
+,"type":"boolean","difficulty":"medium","question":"The Pythagorean theorem states that the square of the hypotenuse is equal to the product of the squares of the other two sides.","correct_answer":"False","incorrect_answers":["True"]}
+  ,{"category":"Science: Mathematics","type":"boolean","difficulty":"medium","question":"The proof for the Chinese Remainder Theorem used in Number Theory was NOT developed by its first publisher, Sun Tzu.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"medium","question":"111,111,111 x 111,111,111 = 12,345,678,987,654,321","correct_answer":"True","incorrect_answers":["False"]}
+  ,{"category":"Science: Mathematics","type":"boolean","difficulty":"easy","question":"The &#039;Squaring the Circle&#039; problem is solvable.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"The Republic of Malta is the smallest microstate worldwide.","correct_answer":"False","incorrect_answers":["True"]},
+  
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"hard","question":"The binary number &quot;101001101&quot; is equivalent to the Decimal number &quot;334&quot;","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"hard","question":"If you could fold a piece of paper in half 50 times, its&#039; thickness will be 3\/4th the distance from the Earth to the Sun.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"Vatican City is a country.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"medium","question":"Zero factorial is equal to zero. ","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"medium","question":"A &#039;Millinillion&#039; is a real number.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"easy","question":"An isosceles triangle has two sides of equal length as opposed to three.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"Ottawa is the capital of Canada.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Mathematics","type":"boolean","difficulty":"easy","question":"A scalene triangle has two sides of equal length.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"The HTML5 standard was published in 2014.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"Tokyo is the capital of Japan.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"MacOS is based on Linux.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"AMD created the first consumer 64-bit processor.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"The Windows 7 operating system has six main editions.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"It&#039;s not possible to format a write-protected DVD-R Hard Disk.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"New Haven is the capital city of the state of Connecticut in the United States.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.","correct_answer":"False","incorrect_answers":["True"]}
+]
+
+
